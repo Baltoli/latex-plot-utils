16a5f88613f8ece5949a6e9301efae0ba4556d51
Bruce Collie <brucecollie82@gmail.com>
2020-04-09 14:43:05 +0100

    Change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="latex-plot-utils-baltoli", # Replace with your own username
+    name="latex-plot-utils", # Replace with your own username
     version="0.0.1",
     author="Bruce Collie",
     author_email="brucecollie82@gmail.com",
