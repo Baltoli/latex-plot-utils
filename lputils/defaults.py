6ef0675fffe48e2b240b8a0fe4f7816a355ec202
Bruce Collie <brucecollie82@gmail.com>
2021-05-11 14:29:47 +0100

    Stop relying on incorrect behaviour

--- lputils/defaults.py
+++ lputils/defaults.py
@@ -4,11 +4,12 @@ SMALL_SIZE = 6
 MEDIUM_SIZE = 8
 BIGGER_SIZE = 10
 
-plt.rc('font', size=SMALL_SIZE)                 # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)            # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)           # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)           # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)           # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)           # legend fontsize
-plt.rc('legend', title_fontsize=MEDIUM_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)         # fontsize of the figure title
+def set():
+    plt.rc('font', size=SMALL_SIZE)                 # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)            # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)           # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)           # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)           # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)           # legend fontsize
+    plt.rc('legend', title_fontsize=MEDIUM_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)         # fontsize of the figure title
